da61f8fef9daf2f78d8d756a63b165c6c68236d0
ravanzli <revanzekeryeli2002@gmail.om>
2023-08-28 16:03:15 +0400

    modified version distinc

--- src/Application/Handlers/Program/Queries/GetAllProgramsQuery.cs
+++ src/Application/Handlers/Program/Queries/GetAllProgramsQuery.cs
@@ -18,7 +18,32 @@ public class GetAllProgramsQueryHandler : IRequestHandler<GetAllProgramsQuery, I
     {
         IEnumerable<Program> programs = await _unitOfWork.ProgramRepository.GetAllAsync(includes: "Modules");
         programs.ToList().ForEach(a => a.Modules = a.Modules.Where(a => a.TopModuleId == null).ToList());
-        var programss = _mapper.Map<IEnumerable<GetAllProgramResponseDto>>(programs);
+        IEnumerable<GetAllProgramResponseDto> programss = programs.Select(p => new GetAllProgramResponseDto()
+        {
+            Id = p.Id,
+            Name = p.Name,
+            TotalHours = p.TotalHours,
+            Modules = p.Modules
+                .OrderBy(m => Version.TryParse(m.Version, out var parsedVersion) ? parsedVersion : null)
+                .Select(p => new GetModuleDto()
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Hours = p.Hours,
+                    Version = p.Version!,
+                    SubModules = p.SubModules?
+                    .OrderBy(m => Version.TryParse(m.Version, out var parsedVersion) ? parsedVersion : null)
+                    .Select(p => new SubModuleDto()
+                    {
+                        Id = p.Id,
+                        Version = p.Version,
+                        Hours = p.Hours,
+                        Name = p.Name,
+                        TopModuleId = p.TopModuleId
+                    })
+                })
+        });
+
         return programss;
     }
 }
