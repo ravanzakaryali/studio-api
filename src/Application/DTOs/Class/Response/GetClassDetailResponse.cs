fe3878e2af9538ad3decf1c2b7dded9c88a7aeff
ravanzli <revanzekeryeli2002@gmail.om>
2023-11-20 16:03:55 +0400

    added total hours in classes get by id

--- src/Application/DTOs/Class/Response/GetClassDetailResponse.cs
+++ src/Application/DTOs/Class/Response/GetClassDetailResponse.cs
@@ -5,6 +5,7 @@ public class GetClassDetailResponse
     public string Name { get; set; } = null!;
     public DateTime? StartDate { get; set; }
     public DateTime? EndDate { get; set; }
+    public int TotalHours { get; set; }
     public GetProgramResponseDto Program { get; set; } = null!;
     public GetSessionResponseDto Session { get; set; } = null!;
     public double AttendanceRate { get; set; }
