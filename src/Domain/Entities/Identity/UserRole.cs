c165f187caaad17762a76f5921c75392b689a09f
ravanzli <revanzekeryeli2002@gmail.om>
2023-12-11 14:14:29 +0400

    modified project

--- src/Domain/Entities/Identity/UserRole.cs
+++ src/Domain/Entities/Identity/UserRole.cs
@@ -2,6 +2,9 @@
 
 public class UserRole : IdentityUserRole<Guid>
 {
+    public Guid Id { get; set; }
+    public override Guid UserId { get; set; }
     public virtual User User { get; set; } = null!;
+    public override Guid RoleId { get; set; }
     public virtual Role Role { get; set; } = null!;
 }
--- src/Infrastructure/Persistence/Configurations/Identity/UserRoleConfiguration.cs
+++ src/Infrastructure/Persistence/Configurations/Identity/UserRoleConfiguration.cs
@@ -5,7 +5,11 @@ public class UserRoleConfiguration : IEntityTypeConfiguration<UserRole>
     public void Configure(EntityTypeBuilder<UserRole> builder)
     {
 
-        builder.ToTable("AspNetUserRoles");
+        builder.ToTable("UserRoles");
+
+        builder.HasKey(e => e.Id);
+        builder.Property(e => e.Id).HasDefaultValueSql("NEWID()");
+
         builder.HasOne(d => d.Role)
             .WithMany(p => p.UserRoles)
             .HasForeignKey(d => d.RoleId)
