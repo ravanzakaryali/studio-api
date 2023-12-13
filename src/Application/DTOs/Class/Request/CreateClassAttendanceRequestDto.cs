a25c6e48ac2e9bda4aaa321c2f4fa889df73605f
ravanzli <revanzekeryeli2002@gmail.om>
2023-12-13 12:57:32 +0400

    modified attendance

--- src/Application/DTOs/Class/Request/CreateClassAttendanceRequestDto.cs
+++ src/Application/DTOs/Class/Request/CreateClassAttendanceRequestDto.cs
@@ -2,7 +2,6 @@
 
 public class CreateClassAttendanceRequestDto
 {
-    public int ModuleId { get; set; }
     public DateOnly Date { get; set; }
     public ICollection<UpdateAttendanceCategorySessionDto> Sessions { get; set; } = null!;
 }
