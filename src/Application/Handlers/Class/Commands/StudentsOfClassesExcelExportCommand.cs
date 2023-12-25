d815bb9db315eea77f903c11348025a3469a9e0f
ravanzli <revanzekeryeli2002@gmail.om>
2023-12-25 10:52:13 +0400

    modified excel export

--- src/Application/Handlers/Class/Commands/StudentsOfClassesExcelExportCommand.cs
+++ src/Application/Handlers/Class/Commands/StudentsOfClassesExcelExportCommand.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using ClosedXML.Excel;
+
 namespace Space.Application.Handlers;
 
 public class StudentsOfClassesExcelExportCommand : IRequest<FileContentResponseDto>
@@ -9,33 +10,122 @@ public class StudentsOfClassesExcelExportCommand : IRequest<FileContentResponseD
     public DateTime? StartDate { get; set; }
     public DateTime? EndDate { get; set; }
 }
-internal class StudentsOfClassesExcelExportHandler : IRequestHandler<StudentsOfClassesExcelExportCommand, FileContentResponseDto>
+
+internal class StudentsOfClassesExcelExportHandler
+    : IRequestHandler<StudentsOfClassesExcelExportCommand, FileContentResponseDto>
 {
     readonly ISpaceDbContext _spaceDbContext;
     readonly IHttpContextAccessor _contextAccessor;
 
-    public StudentsOfClassesExcelExportHandler(ISpaceDbContext spaceDbContext, IHttpContextAccessor contextAccessor)
+    public StudentsOfClassesExcelExportHandler(
+        ISpaceDbContext spaceDbContext,
+        IHttpContextAccessor contextAccessor
+    )
     {
         _spaceDbContext = spaceDbContext;
         _contextAccessor = contextAccessor;
     }
 
-    public async Task<FileContentResponseDto> Handle(StudentsOfClassesExcelExportCommand request, CancellationToken cancellationToken)
+    public async Task<FileContentResponseDto> Handle(
+        StudentsOfClassesExcelExportCommand request,
+        CancellationToken cancellationToken
+    )
     {
-        //IQueryable<Class> query = _spaceDbContext.Classes
-        //    .Include(c => c.ClassTimeSheets)
-        //    .ThenInclude(c => c.Attendances)
-        //    .ThenInclude(c => c.Student)
-        //    .ThenInclude(c => c.Student)
-        //    .ThenInclude(c => c!.Contact)
-        //    .AsQueryable();
-        //query = request.ClassStatus switch
-        //{
-        //    StudyCountStatus.Equal => query.Where(c => c.Studies.Count == request.StudyCount),
-        //    StudyCountStatus.Less => query.Where(c => c.Studies.Count <= request.StudyCount),
-        //    StudyCountStatus.More => query.Where(c => c.Studies.Count >= request.StudyCount),
-        //    _ => query.Where(c => c.Studies.Count == request.StudyCount),
-        //};
-        throw new Exception();
+        IQueryable<Class> query = _spaceDbContext
+            .Classes
+            .Include(c => c.ClassTimeSheets)
+            .ThenInclude(c => c.Attendances)
+            .ThenInclude(c => c.Student)
+            .ThenInclude(c => c.Student)
+            .ThenInclude(c => c!.Contact)
+            .AsQueryable();
+
+        DateOnly dateNow = DateOnly.FromDateTime(DateTime.Now);
+
+        if (request.ClassStatus == ClassStatus.Active)
+        {
+            query = query.Where(c => dateNow > c.StartDate && dateNow < c.EndDate);
+        }
+        else if (request.ClassStatus == ClassStatus.Close)
+        {
+            query = query.Where(c => dateNow > c.EndDate);
+        }
+        else if (request.ClassStatus == ClassStatus.New)
+        {
+            query = query.Where(c => dateNow < c.StartDate && dateNow < c.EndDate);
+        }
+
+        if (request.ClassIds != null && request.ClassIds.Count > 0)
+        {
+            query = query.Where(c => request.ClassIds.Contains(c.Id));
+        }
+        if (request.ProgramIds != null && request.ProgramIds.Count > 0)
+        {
+            query = query.Where(c => request.ProgramIds.Contains(c.ProgramId));
+        }
+        if (request.StartDate != null)
+        {
+            DateOnly startDate = DateOnly.FromDateTime(request.StartDate.Value);
+            query = query.Where(c => c.StartDate >= startDate);
+        }
+        if (request.EndDate != null)
+        {
+            DateOnly endDate = DateOnly.FromDateTime(request.EndDate.Value);
+            query = query.Where(c => c.EndDate <= endDate);
+        }
+        List<Class> classes = await query.ToListAsync(cancellationToken);
+
+        List<StudentExcelExportDto> allStudents = classes
+                .SelectMany(currentClass => currentClass.ClassTimeSheets
+                    .SelectMany(c => c.Attendances
+                        .Select(at => new StudentExcelExportDto()
+                        {
+                            ClassName = currentClass.Name,
+                            Date = c.Date,
+                            Email = at.Student.Student!.Email,
+                            Name = at.Student.Student.Contact!.Name,
+                            Surname = at.Student.Student.Contact.Surname,
+                            TotalHour = at.TotalAttendanceHours
+                        })))
+                .ToList();
+
+        using XLWorkbook workbook = new();
+        IXLWorksheet worksheet = workbook.Worksheets.Add("Davamiyyət göstəriciləri");
+
+        worksheet.Cell(1, 1).Value = "Grup No";
+        worksheet.Cell(1, 2).Value = "Adı";
+        worksheet.Cell(1, 3).Value = "Soyadı";
+        worksheet.Cell(1, 4).Value = "Email";
+        worksheet.Cell(1, 5).Value = "Tarix";
+        worksheet.Cell(1, 6).Value = "İştirak saatı";
+
+        worksheet.Column(2).Width = 10;
+        worksheet.Column(3).Width = 15;
+        worksheet.Column(4).Width = 30;
+        worksheet.Column(5).Width = 15;
+        worksheet.Column(6).Width = 10;
+
+        worksheet.Cell(2, 1).InsertData(allStudents.OrderBy(c => c.Date));
+
+        MemoryStream memoryStream = new();
+        workbook.SaveAs(memoryStream);
+        byte[] excelData = memoryStream.ToArray();
+
+        if (_contextAccessor.HttpContext is null) throw new Exception("Http context is null");
+
+        _contextAccessor.HttpContext.Response.Headers.Add("Content-Disposition", $"attachment; filename=All_Classes_{DateTime.Now:yyyy-MM-dd}.xlsx");
+        _contextAccessor.HttpContext.Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+        await _contextAccessor.HttpContext.Response.Body.WriteAsync(excelData);
+        await _contextAccessor.HttpContext.Response.CompleteAsync();
+
+        FileContentResponseDto file = new()
+        {
+            Name = $"All_Classes_{DateTime.Now:yyyy-MM-dd}.xlsx",
+            ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            FileBytes = excelData
+        };
+
+        return file;
     }
 }
--- src/Application/Handlers/ClassSession/Commands/CreateClassSessionAttendanceCommand.cs
+++ src/Application/Handlers/ClassSession/Commands/CreateClassSessionAttendanceCommand.cs
@@ -8,65 +8,77 @@ public class CreateClassSessionAttendanceCommand : IRequest
     public ICollection<UpdateAttendanceCategorySessionDto> Sessions { get; set; } = null!;
 }
 
-internal class UpdateClassSessionAttendanceCommandHandler : IRequestHandler<CreateClassSessionAttendanceCommand>
+internal class UpdateClassSessionAttendanceCommandHandler
+    : IRequestHandler<CreateClassSessionAttendanceCommand>
 {
     readonly ISpaceDbContext _spaceDbContext;
     readonly IUnitOfWork _unitOfWork;
 
     public UpdateClassSessionAttendanceCommandHandler(
         ISpaceDbContext spaceDbContext,
-        IUnitOfWork unitOfWork)
+        IUnitOfWork unitOfWork
+    )
     {
         _spaceDbContext = spaceDbContext;
         _unitOfWork = unitOfWork;
     }
 
-    public async Task Handle(CreateClassSessionAttendanceCommand request, CancellationToken cancellationToken)
+    public async Task Handle(
+        CreateClassSessionAttendanceCommand request,
+        CancellationToken cancellationToken
+    )
     {
-        Class @class = await _spaceDbContext.Classes
-          .Include(c => c.Studies)
-          .Include(c => c.Program)
-          .ThenInclude(c => c.Modules)
-          .ThenInclude(c => c.SubModules)
-          .Include(c => c.ClassModulesWorkers)
-          .ThenInclude(c => c.Worker)
-          .Include(c => c.ClassModulesWorkers)
-          .ThenInclude(c => c.Role)
-          .Where(c => c.Id == request.ClassId)
-          .FirstOrDefaultAsync(cancellationToken: cancellationToken) ??
-              throw new NotFoundException(nameof(Class), request.ClassId);
+        Class @class =
+            await _spaceDbContext
+                .Classes
+                .Include(c => c.Studies)
+                .Include(c => c.Program)
+                .ThenInclude(c => c.Modules)
+                .ThenInclude(c => c.SubModules)
+                .Include(c => c.ClassModulesWorkers)
+                .ThenInclude(c => c.Worker)
+                .Include(c => c.ClassModulesWorkers)
+                .ThenInclude(c => c.Role)
+                .Where(c => c.Id == request.ClassId)
+                .FirstOrDefaultAsync(cancellationToken: cancellationToken)
+            ?? throw new NotFoundException(nameof(Class), request.ClassId);
 
         //həmin günün class sessiona bax
-        List<ClassSession> classSessions = await _spaceDbContext.ClassSessions
+        List<ClassSession> classSessions = await _spaceDbContext
+            .ClassSessions
             .Where(c => c.Date == request.Date && c.ClassId == request.ClassId)
             .ToListAsync(cancellationToken: cancellationToken);
-
-
 
         //əgər yoxdursa o zaman error qaytar
         if (request.HeldModules != null)
         {
             List<int> requestModuleIds = request.HeldModules.Select(c => c.Id).ToList();
-            if (classSessions.Count != requestModuleIds.Count) throw new NotFoundException("Module not found");
-            List<Module> module = await _spaceDbContext.Modules
-            .Where(m => requestModuleIds.Contains(m.Id))
-            .ToListAsync(cancellationToken: cancellationToken);
-            if (requestModuleIds.Count != module.Count) throw new NotFoundException("Modules not found");
+            // if (classSessions.Count != requestModuleIds.Count)
+            //     throw new NotFoundException("Module not found");
+            List<Module> module = await _spaceDbContext
+                .Modules
+                .Where(m => requestModuleIds.Contains(m.Id))
+                .ToListAsync(cancellationToken: cancellationToken);
+                
+            if (requestModuleIds.Count != module.Count)
+                throw new NotFoundException("Modules not found");
         }
 
         //ders cancelled olursa bu işləməlidi
         List<DateOnly> holidayDates = await _unitOfWork.HolidayService.GetDatesAsync();
         DateOnly classLastDate = await _unitOfWork.ClassSessionService.GetLastDateAsync(@class.Id);
 
+        List<int> studentIds = request
+            .Sessions
+            .SelectMany(s => s.Attendances)
+            .Select(a => a.StudentId)
+            .ToList();
 
-        List<int> studentIds = request.Sessions
-                .SelectMany(s => s.Attendances)
-                .Select(a => a.StudentId)
-                .ToList();
+        if (@class.Studies.Any(c => !studentIds.Contains(c.Id)))
+            throw new NotFoundException("Student not found");
 
-        if (@class.Studies.Any(c => !studentIds.Contains(c.Id))) throw new NotFoundException("Student not found");
-
-        List<ClassTimeSheet> classTimeSheets = await _spaceDbContext.ClassTimeSheets
+        List<ClassTimeSheet> classTimeSheets = await _spaceDbContext
+            .ClassTimeSheets
             .Where(c => c.Date == request.Date && c.ClassId == request.ClassId)
             .ToListAsync(cancellationToken: cancellationToken);
 
@@ -76,84 +88,118 @@ internal class UpdateClassSessionAttendanceCommandHandler : IRequestHandler<Crea
         List<ClassTimeSheet> addTimeSheets = new();
         foreach (UpdateAttendanceCategorySessionDto session in request.Sessions)
         {
-            ClassSession? classSession = classSessions.Where(cs => cs.Category == session.Category).FirstOrDefault();
-            if (classSession is null) continue;
+            ClassSession? classSession = classSessions
+                .Where(cs => cs.Category == session.Category)
+                .FirstOrDefault();
+            if (classSession is null)
+                continue;
 
             if (session.AttendancesWorkers.Any(c => c.TotalMinutes >= 60))
                 throw new ValidationException("It cannot be more than 60 minutes");
 
             if (session.AttendancesWorkers.Any(c => c.TotalHours > classSession.TotalHours))
-                throw new ValidationException("The worker's time cannot be longer than the lesson's time");
+                throw new ValidationException(
+                    "The worker's time cannot be longer than the lesson's time"
+                );
 
             if (session.Attendances.Any(c => c.TotalAttendanceHours > classSession.TotalHours))
-                throw new ValidationException("The student's time cannot be longer than the lesson's time");
+                throw new ValidationException(
+                    "The student's time cannot be longer than the lesson's time"
+                );
 
             if (classSession.Status != ClassSessionStatus.Cancelled)
             {
-                ClassTimeSheet classTimeSheet = new()
-                {
-                    Attendances = session.Attendances.Select(c => new Attendance()
+                ClassTimeSheet classTimeSheet =
+                    new()
                     {
-                        StudyId = c.StudentId,
-                        Note = c.Note,
-                        Status = classSession.TotalHours == c.TotalAttendanceHours
-                                           ? StudentStatus.Attended
-                                           : c.TotalAttendanceHours == 0
-                                           ? StudentStatus.Absent
-                                           : StudentStatus.Partial,
-                        TotalAttendanceHours = c.TotalAttendanceHours
-                    }).ToList(),
-                    AttendancesWorkers = session.AttendancesWorkers.Select(wa => new AttendanceWorker()
-                    {
-                        WorkerId = wa.WorkerId,
-                        TotalHours = wa.TotalHours,
-                        TotalMinutes = wa.TotalMinutes,
-                        RoleId = wa.RoleId,
-                        AttendanceStatus = wa.AttendanceStatus,
-                    }).ToList(),
-                    TotalHours = classSession.TotalHours,
-                    Category = classSession.Category,
-                    ClassId = classSession.ClassId,
-                    EndTime = classSession.EndTime,
-                    Date = classSession.Date,
-
-                    StartTime = classSession.StartTime,
-                    Status = classSession.Status,
-                };
+                        Attendances = session
+                            .Attendances
+                            .Select(
+                                c =>
+                                    new Attendance()
+                                    {
+                                        StudyId = c.StudentId,
+                                        Note = c.Note,
+                                        Status =
+                                            classSession.TotalHours == c.TotalAttendanceHours
+                                                ? StudentStatus.Attended
+                                                : c.TotalAttendanceHours == 0
+                                                    ? StudentStatus.Absent
+                                                    : StudentStatus.Partial,
+                                        TotalAttendanceHours = c.TotalAttendanceHours
+                                    }
+                            )
+                            .ToList(),
+                        AttendancesWorkers = session
+                            .AttendancesWorkers
+                            .Select(
+                                wa =>
+                                    new AttendanceWorker()
+                                    {
+                                        WorkerId = wa.WorkerId,
+                                        TotalHours = wa.TotalHours,
+                                        TotalMinutes = wa.TotalMinutes,
+                                        RoleId = wa.RoleId,
+                                        AttendanceStatus = wa.AttendanceStatus,
+                                    }
+                            )
+                            .ToList(),
+                        TotalHours = classSession.TotalHours,
+                        Category = classSession.Category,
+                        ClassId = classSession.ClassId,
+                        EndTime = classSession.EndTime,
+                        Date = classSession.Date,
+                        StartTime = classSession.StartTime,
+                        Status = classSession.Status,
+                    };
                 if (session.Category == ClassSessionCategory.Theoric && request.HeldModules != null)
-                    classTimeSheet.HeldModules = request.HeldModules.Select(hm => new HeldModule()
-                    {
-                        ModuleId = hm.Id,
-                        TotalHours = hm.TotalHours,
-                    }).ToList();
+                    classTimeSheet.HeldModules = request
+                        .HeldModules
+                        .Select(
+                            hm => new HeldModule() { ModuleId = hm.Id, TotalHours = hm.TotalHours, }
+                        )
+                        .ToList();
                 addTimeSheets.Add(classTimeSheet);
             }
             else
             {
                 session.Status = ClassSessionStatus.Cancelled;
 
-
                 DateOnly date2 = classLastDate.AddDays(1);
                 DayOfWeek startDateDayOfWeek = date2.DayOfWeek;
-                while (!@class.Session.Details.Select(c => c.DayOfWeek).Any(c => date2.DayOfWeek == c))
+                while (
+                    !@class.Session.Details.Select(c => c.DayOfWeek).Any(c => date2.DayOfWeek == c)
+                )
                 {
                     date2 = date2.AddDays(1);
                     startDateDayOfWeek = date2.DayOfWeek;
                 }
-                List<ClassSession> generateClassSessions = _unitOfWork.ClassSessionService.GenerateSessions(
-                    classSession.TotalHours, request.Sessions.Select(r => new CreateClassSessionDto()
-                    {
-                        Category = r.Category,
-                        DayOfWeek = startDateDayOfWeek,
-                        End = classSession.EndTime,
-                        Start = classSession.StartTime
-                    }).ToList(),
-                    date2,
-                    holidayDates,
-                @class.Id,
-                    classSession.RoomId!.Value);
+                List<ClassSession> generateClassSessions = _unitOfWork
+                    .ClassSessionService
+                    .GenerateSessions(
+                        classSession.TotalHours,
+                        request
+                            .Sessions
+                            .Select(
+                                r =>
+                                    new CreateClassSessionDto()
+                                    {
+                                        Category = r.Category,
+                                        DayOfWeek = startDateDayOfWeek,
+                                        End = classSession.EndTime,
+                                        Start = classSession.StartTime
+                                    }
+                            )
+                            .ToList(),
+                        date2,
+                        holidayDates,
+                        @class.Id,
+                        classSession.RoomId!.Value
+                    );
 
-                await _spaceDbContext.ClassSessions.AddRangeAsync(generateClassSessions, cancellationToken);
+                await _spaceDbContext
+                    .ClassSessions
+                    .AddRangeAsync(generateClassSessions, cancellationToken);
             }
         }
         _spaceDbContext.ClassTimeSheets.AddRange(addTimeSheets);
