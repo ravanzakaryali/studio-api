09cee611ff560809030b9a4458f229898ca36c85
ravanzakaryali <revanzekeryeli2002@gmail.com>
2024-01-10 11:39:26 +0400

    modified pul class modules workers

--- src/Application/DTOs/Class/Request/UpdateClassModuleRequestDto.cs
+++ src/Application/DTOs/Class/Request/UpdateClassModuleRequestDto.cs
@@ -2,6 +2,6 @@ namespace Space.Application.DTOs;
 public class UpdateClassModuleRequestDto
 {
     public IEnumerable<CreateClassModuleRequestDto> Modules { get; set; } = null!;
-    public IEnumerable<CreateClassExtraModuleRequestDto> ExtraModules { get; set; } = null!;
-    public IEnumerable<CreateClassNewExtraModuleRequestDto> NewExtraModules { get; set; } = null!;
+    public IEnumerable<CreateClassExtraModuleRequestDto>? ExtraModules { get; set; }
+    public IEnumerable<CreateClassNewExtraModuleRequestDto>? NewExtraModules { get; set; }
 }
