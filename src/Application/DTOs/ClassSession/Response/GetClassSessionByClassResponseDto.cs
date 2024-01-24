ace5931c1c10433c7d8a1b3192ca2596d06b6e32
ravanzakaryali <revanzekeryeli2002@gmail.com>
2024-01-24 12:36:57 +0400

    modified hours

--- src/Application/DTOs/ClassSession/Response/GetClassSessionByClassResponseDto.cs
+++ src/Application/DTOs/ClassSession/Response/GetClassSessionByClassResponseDto.cs
@@ -8,3 +8,12 @@ public class GetClassSessionByClassResponseDto
     public TimeOnly EndTime { get; set;}
     public int TotalHours { get; set; }
 }
+
+public class GetClassSessionByClassByDayResponseDto
+{
+    public string ClassName { get; set; } = null!;
+    public ClassSessionCategory? Category { get; set; }
+    public TimeOnly StartTime { get; set; }
+    public TimeOnly EndTime { get; set;}
+    public int Hours { get; set; }
+}
--- src/Application/Handlers/Class/Queries/GetClassSessionByClassQuery.cs
+++ src/Application/Handlers/Class/Queries/GetClassSessionByClassQuery.cs
@@ -1,7 +1,7 @@
 ﻿namespace Space.Application.Handlers;
 
-public record GetClassSessionByClassQuery(int Id, DateTime Date) : IRequest<IEnumerable<GetClassSessionByClassResponseDto>>;
-internal class GetClassSessionByClassQueryHandler : IRequestHandler<GetClassSessionByClassQuery, IEnumerable<GetClassSessionByClassResponseDto>>
+public record GetClassSessionByClassQuery(int Id, DateTime Date) : IRequest<IEnumerable<GetClassSessionClassByDayResponseDto>>;
+internal class GetClassSessionByClassQueryHandler : IRequestHandler<GetClassSessionByClassQuery, IEnumerable<GetClassSessionClassByDayResponseDto>>
 {
     readonly ISpaceDbContext _spaceDbContext;
 
@@ -11,7 +11,7 @@ internal class GetClassSessionByClassQueryHandler : IRequestHandler<GetClassSess
         _spaceDbContext = spaceDbContext;
     }
 
-    public async Task<IEnumerable<GetClassSessionByClassResponseDto>> Handle(GetClassSessionByClassQuery request, CancellationToken cancellationToken)
+    public async Task<IEnumerable<GetClassSessionClassByDayResponseDto>> Handle(GetClassSessionByClassQuery request, CancellationToken cancellationToken)
     {
         Class? @class = await _spaceDbContext.Classes
             .Where(c => c.Id == request.Id)
@@ -23,13 +23,13 @@ internal class GetClassSessionByClassQueryHandler : IRequestHandler<GetClassSess
 
         List<ClassSession> classSessions = @class.ClassSessions.Where(c => c.Date == requestDate).ToList();
         int totalHour = classSessions.Sum(c => c.TotalHours);
-        return classSessions.Select(session => new GetClassSessionByClassResponseDto()
+        return classSessions.Select(session => new GetClassSessionClassByDayResponseDto()
         {
             ClassName = @class.Name,
             Category = session.Category,
             EndTime = session.EndTime,
             StartTime = session.StartTime,
-            TotalHours = totalHour,
+            Hours = totalHour,
         });
     }
 }
