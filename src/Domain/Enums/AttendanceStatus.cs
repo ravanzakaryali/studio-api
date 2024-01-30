8373c7f1b6aa09a600ec19dad65fca2726a181d6
ravanzakaryali <revanzekeryeli2002@gmail.com>
2024-01-30 12:51:55 +0400

    modified:

--- src/Domain/Enums/AttendanceStatus.cs
+++ src/Domain/Enums/AttendanceStatus.cs
@@ -4,13 +4,8 @@ namespace Space.Domain.Enums;
 
 public enum AttendanceStatus
 {
-    [Description("Tam iştirak")]
     Full = 1,
-    [Description("Gecikmə")]
     Late = 2,
-    [Description("Erkən çıxma")]
     Early = 3,
-
-    [Description("İştirak etməyib")]
     Absent = 4,
 }
