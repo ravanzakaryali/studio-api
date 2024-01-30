4b4efdd296af12625e42b30c84196bb8de4d2a15
ravanzakaryali <revanzekeryeli2002@gmail.com>
2024-01-30 07:56:52 +0400

    modifed

--- src/Domain/Entities/Identity/UserRole.cs
+++ src/Domain/Entities/Identity/UserRole.cs
@@ -2,6 +2,7 @@
 
 public class UserRole : IdentityUserRole<Guid>
 {
+    public Guid Id { get; set; }
     public override Guid UserId { get; set; }
     public virtual User User { get; set; }
     public override Guid RoleId { get; set; }
---
+++ src/Infrastructure/Persistence/Migrations/20240130034224_Change_RoleProblem.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Space.Infrastructure.Persistence.Migrations
+{
+    public partial class Change_RoleProblem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserRoles_Roles_RoleId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserRoles_Users_UserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AspNetUserRoles",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUserRoles",
+                newName: "UserRoles");
+
+            // migrationBuilder.RenameIndex(
+            //     name: "IX_AspNetUserRoles_RoleId",
+            //     table: "UserRoles",
+            //     newName: "IX_UserRoles_RoleId");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Id",
+                table: "UserRoles",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserRoles",
+                table: "UserRoles",
+                columns: new[] { "UserId", "RoleId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRoles_Roles_RoleId",
+                table: "UserRoles",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRoles_Users_UserId",
+                table: "UserRoles",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRoles_Roles_RoleId",
+                table: "UserRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRoles_Users_UserId",
+                table: "UserRoles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserRoles",
+                table: "UserRoles");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "UserRoles");
+
+            migrationBuilder.RenameTable(
+                name: "UserRoles",
+                newName: "AspNetUserRoles");
+
+            // migrationBuilder.RenameIndex(
+            //     name: "IX_UserRoles_RoleId",
+            //     table: "AspNetUserRoles",
+            //     newName: "IX_AspNetUserRoles_RoleId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AspNetUserRoles",
+                table: "AspNetUserRoles",
+                columns: new[] { "UserId", "RoleId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserRoles_Roles_RoleId",
+                table: "AspNetUserRoles",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserRoles_Users_UserId",
+                table: "AspNetUserRoles",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
