a020a75e202b92a6719950d2f83b2040990305ed
ravanzakaryali <revanzekeryeli2002@gmail.com>
2024-03-30 13:55:11 +0400

    STD-1/STD-13

--- src/Application/Handlers/Worker/Commands/DeleteInstructorCommand.cs
+++ src/Application/Handlers/Worker/Commands/DeleteInstructorCommand.cs
@@ -18,8 +18,26 @@ internal class DeleteWorkerCommandHandler : IRequestHandler<DeleteWorkerCommand,
         Worker? Worker = await _spaceDbContext.Workers.FindAsync(request.Id) ??
             throw new NotFoundException(nameof(Worker), request.Id);
         //Todo: support delete
+
+
+        List<ClassModulesWorker> classModuleWorkers = await _spaceDbContext.ClassModulesWorkers
+                            .Where(x => x.WorkerId == Worker.Id)
+                            .Include(c => c.Class)
+                            .ToListAsync();
+
+        DateOnly dateNow = DateOnly.FromDateTime(DateTime.Now);
+        foreach (ClassModulesWorker item in classModuleWorkers)
+        {
+            if (item.Class.EndDate >= dateNow)
+            {
+                //Todo: Exception handler
+                throw new Exception("Worker is assigned to a class that is not finished yet");
+            }
+        }
+
         _spaceDbContext.Supports.RemoveRange(await _spaceDbContext.Supports.Where(x => x.UserId == Worker.Id).ToListAsync(cancellationToken: cancellationToken));
         _spaceDbContext.Workers.Remove(Worker);
+
         await _spaceDbContext.SaveChangesAsync(cancellationToken);
         return _mapper.Map<GetWorkerResponseDto>(Worker);
     }
