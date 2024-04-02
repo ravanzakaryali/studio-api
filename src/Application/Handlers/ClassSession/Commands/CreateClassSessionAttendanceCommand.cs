16075a9a2511148f503f2366e82fd3bd4fbaad90
ravanzakaryali <revanzekeryeli2002@gmail.com>
2024-04-02 16:06:18 +0400

    modified

--- src/Application/Handlers/ClassSession/Commands/CreateClassSessionAttendanceCommand.cs
+++ src/Application/Handlers/ClassSession/Commands/CreateClassSessionAttendanceCommand.cs
@@ -97,21 +97,13 @@ internal class UpdateClassSessionAttendanceCommandHandler
             ClassSession? classSessionFirst = classSessions.FirstOrDefault() ?? throw new NotFoundException("Class session not found");
             ClassSession? classSession = classSessions
                 .Where(cs => cs.Category == session.Category)
-                .FirstOrDefault() ?? new ClassSession()
-                {
-                    Category = session.Category,
-                    ClassId = @class.Id,
-                    Date = dateNow,
-                    TotalHours = classSessionFirst.TotalHours,
-                    EndTime = classSessionFirst.EndTime,
-                    StartTime = classSessionFirst.StartTime,
-                    CreatedBy = classSessionFirst.CreatedBy,
-                    CreatedDate = classSessionFirst.CreatedDate,
-                    RoomId = classSessionFirst.RoomId,
-                    Status = classSessionFirst.Status,
-                    LastModifiedBy = classSessionFirst.LastModifiedBy,
-                    LastModifiedDate = classSessionFirst.LastModifiedDate,
-                };
+                .FirstOrDefault();
+
+            if (classSession == null)
+            {
+                classSession = classSessionFirst;
+                classSession.Category = session.Category;
+            }
             @classSession.RoomId ??= @class.RoomId;
 
             classSession.Status = session.Status;
