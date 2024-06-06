9ebb5b0c417f6156f367431d71b86a10a10c56ef
ravanzakaryali <revanzekeryeli2002@gmail.com>
2024-06-06 11:04:03 +0400

    added permission group access command

---
+++ src/Application/DTOs/AppModules/Request/UpdatePermissionAppModuleDto.cs
@@ -0,0 +1,7 @@
+namespace Space.Application.DTOs;
+
+public class UpdatePermissionAppModuleDto
+{
+    public int Id { get; set; }
+    public ICollection<PermissionAccessLevelDto> Accesses { get; set; } = null!;
+}
