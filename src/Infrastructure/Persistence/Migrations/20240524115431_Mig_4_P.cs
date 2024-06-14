9a68bf10fa1f86c826d1fe61fdaa4f33e76617e8
ravanzakaryali <revanzekeryeli2002@gmail.com>
2024-06-14 12:44:08 +0400

    modified

---
+++ src/Infrastructure/Persistence/Migrations/20240524115431_Mig_4_P.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Space.Infrastructure.Persistence.Migrations
+{
+    public partial class Mig_4_P : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AvatarColor",
+                table: "Workers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvatarColor",
+                table: "Workers");
+        }
+    }
+}
