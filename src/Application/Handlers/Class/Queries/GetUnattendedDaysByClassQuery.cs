5fd1c57400ecdd74c6a79aefb0041ed44c87d606
ravanzakaryali <revanzekeryeli2002@gmail.com>
2024-07-17 11:03:06 +0400

    modified unmarked attendance rate

--- src/Application/Handlers/Class/Queries/GetUnattendedDaysByClassQuery.cs
+++ src/Application/Handlers/Class/Queries/GetUnattendedDaysByClassQuery.cs
@@ -1,10 +1,10 @@
-﻿
-namespace Space.Application.Handlers;
+﻿namespace Space.Application.Handlers;
 
 public class GetUnattendedDaysByClassQuery : IRequest<IEnumerable<GetUnAttendedByClassDto>>
 {
     public int Id { get; set; }
 }
+
 internal class GetUnattendedDaysByClassHandler : IRequestHandler<GetUnattendedDaysByClassQuery, IEnumerable<GetUnAttendedByClassDto>>
 {
     readonly ISpaceDbContext _spaceDbContext;
@@ -17,21 +17,41 @@ internal class GetUnattendedDaysByClassHandler : IRequestHandler<GetUnattendedDa
     public async Task<IEnumerable<GetUnAttendedByClassDto>> Handle(GetUnattendedDaysByClassQuery request, CancellationToken cancellationToken)
     {
         Class @class = await _spaceDbContext.Classes
+            .Include(c => c.Session)
+            .ThenInclude(s => s.Details)
             .Where(c => c.Id == request.Id)
             .FirstOrDefaultAsync(cancellationToken: cancellationToken) ??
                 throw new NotFoundException(nameof(Class), request.Id);
 
         DateOnly dateNow = DateOnly.FromDateTime(DateTime.Now);
 
-        List<DateOnly> classSessionDates = await _spaceDbContext.ClassSessions
-            .Include(c => c.ClassTimeSheet)
-            .Where(c => c.ClassId == @class.Id && c.Date < dateNow && c.ClassTimeSheetId == null && c.Status != ClassSessionStatus.Cancelled)
-            .Select(c => c.Date)
-            .ToListAsync(cancellationToken: cancellationToken);
+        DateOnly startDateClass = @class.StartDate;
+        DateOnly endDateClass = @class.EndDate.HasValue ? @class.EndDate.Value : dateNow;
 
-        return classSessionDates.Select(c => new GetUnAttendedByClassDto()
+        List<ClassTimeSheet> classTimeSheets = await _spaceDbContext.ClassTimeSheets
+            .Where(cts => cts.ClassId == @class.Id && cts.Date < dateNow)
+            .ToListAsync(cancellationToken);
+
+        List<DateOnly> relevantDates = new();
+        
+        for (DateOnly date = startDateClass; date <= endDateClass; date = date.AddDays(1))
         {
-            Date = c
-        }).DistinctBy(c => c.Date).OrderByDescending(c => c.Date);
+            DayOfWeek dayOfWeek = date.DayOfWeek;
+            bool hasSession = @class.Session.Details.Any(sd => sd.DayOfWeek == dayOfWeek);
+            if (hasSession)
+            {
+                relevantDates.Add(date);
+            }
+        }
+
+        List<DateOnly> unattendedDates = relevantDates
+            .Where(date => !classTimeSheets.Any(cts => cts.Date == date) && date < dateNow)
+            .ToList();
+
+        return unattendedDates.Select(date => new GetUnAttendedByClassDto()
+        {
+            Date = date
+        })
+        .OrderByDescending(c => c.Date);
     }
 }
