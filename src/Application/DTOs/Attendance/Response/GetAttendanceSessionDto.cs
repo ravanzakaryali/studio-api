9d3feb1ac4fedb13888ea44cc49f9b34e4d7c484
ravanzakaryali <revanzekeryeli2002@gmail.com>
2024-08-16 16:29:55 +0400

    modified

--- src/Application/DTOs/Attendance/Response/GetAttendanceSessionDto.cs
+++ src/Application/DTOs/Attendance/Response/GetAttendanceSessionDto.cs
@@ -1,7 +1,7 @@
 public class GetAttendanceSessionDto
 {
     public GetClassAttendanceDto Class { get; set; } = null!;
-    public TimeOnly StartTime { get; set; }
+    public TimeOnly? StartTime { get; set; }
     public TimeOnly? EndTime { get; set; }
     public int ClassTimeSheetId { get; set; }
     public int TotalHours { get; set; }
